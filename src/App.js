caea555a7ed2233acb4abd5095f2c5ad8cf56d03
Adrian <adrianrossmiles@gmail.com>
2021-03-18 04:58:48 +0200

    feature branch take two

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Feature branch
+          Learn React feature branch!
         </a>
       </header>
     </div>
